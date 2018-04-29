eacca998a0213e0e12af134fb8c2989c6348c04b
Vlad May <enity.w@gmail.com>
2018-04-29 15:54:16 +0300

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Webpack template
+
+## Whats included
+
+1. webpack + dev server
+1. typescript
+1. tslint
+1. jest + jest-ts
+
+### Scripts
+
+```sh
+npm start
+npm run test
+npm run build
+```
+
---
+++ config/webpack.conf.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+    clientLogLevel: 'warning',
+    stats: 'minimal'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },  // creates style nodes from JS strings
+          { loader: 'css-loader' },// translates CSS into CommonJS
+          { loader: 'sass-loader' } // compiles Sass to CSS
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
